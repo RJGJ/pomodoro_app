6270c04463d01d5c9e7909fb9f9c657b2240ec38
RJGJ <jaictinrj@gmail.com>
2023-07-19 00:28:52 +0800

    removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:pomodoro_app/router.dart';
-import 'package:pomodoro_app/screens/loading_screen.dart';
-import 'package:pomodoro_app/screens/feed_screen.dart';
 
 void main() {
   runApp(const MyApp());
